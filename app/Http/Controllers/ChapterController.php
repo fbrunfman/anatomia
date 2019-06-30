dfd5dc5140e561169e7e858ad2bf37a0cf5f37f8
Sebastian Saenz <ssaenz@multeo.net>
2019-06-29 23:33:29 -0300

    Agrego controladores y sus rutas

--- app/Http/Controllers/ChapterController.php
+++ app/Http/Controllers/ChapterController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Chapter;
 use Illuminate\Http\Request;
 
 class ChapterController extends Controller
 {
-    //
+    public function index()
+    {
+        $chapters = Chapter::all();
+
+        return response()->json([
+            'code' => 200,
+            'data' => $chapters
+        ]);
+    }
 }
--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Lesson;
 use Illuminate\Http\Request;
 
 class LessonController extends Controller
 {
-    //
+    public function index()
+    {
+        $lessons = Lesson::with('chapters')->get();
+
+        return response()->json([
+            'code' => 200,
+            'data' => $lessons
+        ]);
+    }
 }
