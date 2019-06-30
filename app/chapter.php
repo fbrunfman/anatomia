96c4bdebff51bab2f4b59df728e15fcf171ebb5b
Sebastian Saenz <ssaenz@multeo.net>
2019-06-29 23:34:08 -0300

    Creo modelos

--- app/chapter.php
+++ app/chapter.php
@@ -4,7 +4,19 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class chapter extends Model
+class Chapter extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'lesson_id'
+    ];
+
+    public function lesson()
+    {
+        return $this->belongsTo('App\Lesson');
+    }
 }
--- app/lesson.php
+++ app/lesson.php
@@ -4,7 +4,19 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class lesson extends Model
+class Lesson extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    public function chapters()
+    {
+        return $this->hasMany('App\Chapter');
+    }
 }
